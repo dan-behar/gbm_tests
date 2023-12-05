c5b88b8f86d6dc516b9b3798c9b9557d8f47f34d
Daniel Behar Aldana <danielbehar@ufm.edu>
2023-12-04 21:29:49 -0600

    comentarios a codigo e iniciando la exploracion del dataset

--- ejercicio1/palin.py
+++ ejercicio1/palin.py
@@ -1,4 +1,11 @@
 def verifier(palabra):
+    """
+    Function that checks if a word is palidrome or not
+    Inputs:
+        palabra (str): the word to be checked
+    Output:
+        A boolean indicating if it palindrome or not
+    """
     if str(palabra.lower()) == str(palabra.lower())[::-1]:
         print("Si es palindroma")
         return True
--- ejercicio3/jumps.py
+++ ejercicio3/jumps.py
@@ -1,19 +1,36 @@
 def jump(num, step = 0, count = 0):
+    """
+    Recursive function that returns the required steps to reach an specific X number.
+    The only movements allowed are:
+        y + k, where y is the actual position and k the immediate next number
+        y - 1, where y is the actual position
+
+    Inputs:
+        - num (int): the objetive number to reach
+        - step (int): the actual position
+        - count (int): the count of steps that the program has taken so far
+    
+    Outputs:
+        - count (int): the count of steps that the program took to reach the objective
+    """
     # Caso base
     if step == num:
         return count
     
     # Recursividad
+    # If the initial steps are negative
     elif step < 0:
         count = step * - 1
         step = 0
         return jump(num, step, count)
 
+    # If the actual step is less than the half of the objective
     elif step <= num / 2:
         step = step + (step + 1)
         count = count + 1
         return jump(num, step, count)
     
+    # If the actual step is higher than the half of the objective
     else:
         step = step - 1
         count = count + 1
